bab3ba59966df8bec1b09313b5893e3f4537b3f0
artuurlz <arturcontatop@gmail.com>
2022-12-12 17:37:50 -0300

    Criação da section contact e footer

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ if(navClose){
         navMenu.classList.remove('show-menu');
     })
 }
+
+// animação pra aparecer o botão de subir a página e a sombra do header
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('pageUp')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// tirar o menu quando clicar no link
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
