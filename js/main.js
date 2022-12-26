c5bd05091f3b100c183143ba706c69484af07713
artuurlz <arturcontatop@gmail.com>
2022-12-26 10:56:25 -0300

    Atualização do js no envio da mensagem

--- js/main.js
+++ js/main.js
@@ -38,3 +38,31 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// simulação de envio de mensagem
+
+const btnSubmit = document.querySelector('#contact-form-button');
+
+if(btnSubmit.addEventListener('click', () => {
+    var nomeUser = document.querySelector('#name').value;
+    var emailUser = document.querySelector('#email').value;
+    var msgUser = document.querySelector('#msg').value;
+
+    limparInput = () =>{
+        document.querySelector('#name').value={};
+        document.querySelector('#email').value={};
+        document.querySelector('#msg').value={};
+    };
+
+    if(nomeUser !== '' && emailUser !== '' && msgUser !== ''){
+        alert(`Olá ${nomeUser}, sua mensagem foi enviada com sucesso! Em breve entraremos em contato.`);
+    } else if (nomeUser === ''){
+        alert(`Dados incompletos, por favor coloque um nome válido!`);
+    } else if (emailUser === ''){
+        alert(`Dados incompletos, por favor coloque um e-mail válido!`);
+    } else if (msgUser === ''){
+        alert(`Dados incompletos, por favor coloque uma mensagem válida!`);
+    } else {
+        alert('Dados incompletos, por favor coloque todos os dados para o envio!');
+    };
+}));
